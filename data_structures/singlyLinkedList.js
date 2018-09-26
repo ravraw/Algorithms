07e252f7d3e98bb449f72e82082e67f194666b9b
ravindra rawat <ravinrawat.wd@gmail.com>
2018-09-26 14:38:23 -0600

    singleLinkedList : get and set

--- data_structures/singlyLinkedList.js
+++ data_structures/singlyLinkedList.js
@@ -69,16 +69,36 @@ class singlyLinkedList {
     this.length++;
     return this.head;
   }
+
+  get(index) {
+    if (index >= this.length || index < 0) return null;
+    let currentIndex = 0;
+    let currentNode = this.head;
+    while (currentIndex !== index) {
+      currentIndex++;
+      currentNode = currentNode.next;
+      console.log("Looooppping .......");
+    }
+    return currentNode;
+  }
+  set(index, val) {
+    const foundIndex = this.get(index);
+    if (foundIndex) {
+      foundIndex.val = val;
+      return true;
+    }
+    return false;
+  }
 }
 
 // Methods in use
 
 const list = new singlyLinkedList();
-// list.push("Hi");
-// list.push("There");
-// list.push("How");
-// list.push("are");
-// list.push("you");
+list.push("Hi");
+list.push("There");
+list.push("How");
+list.push("are");
+list.push("you");
 
 ///
 console.log("-----------------------------");
@@ -88,13 +108,10 @@ console.log("-----------------------------");
 // console.log("pop##"list.pop());
 // console.log("push##", list.push("rav"));
 // console.log("shift##", list.shift());
-console.log("unshift##", list.unshift("raw"));
+// console.log("unshift##", list.unshift("raw"));
+//console.log("get ##", list.get(3));
+console.log("set ##", list.set(3, "new Value"));
 
-console.log("-----------------------------");
-console.log(list);
-console.log("-----------------------------");
-
-console.log("unshift##", list.unshift("ravi"));
 console.log("-----------------------------");
 console.log(list);
 console.log("-----------------------------");
