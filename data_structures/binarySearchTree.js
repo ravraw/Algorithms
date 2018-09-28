ba590cebbbd6fa8b1103f9d97f342d88676e2935
ravindra rawat <ravinrawat.wd@gmail.com>
2018-09-28 10:17:43 -0600

    binarySearchTree : insert,find,contains

---
+++ data_structures/binarySearchTree.js
@@ -0,0 +1,72 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+class Bst {
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+    const newNode = new Node();
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (val === current.val) return undefined;
+        if (val < current.val) {
+          if (!current.left) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (!current.right) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+const tree = new Bst();
