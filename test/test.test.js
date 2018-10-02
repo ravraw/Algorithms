3d965848d73f9ab03a158bc23db585624c4b55c1
ravindra rawat <ravinrawat.wd@gmail.com>
2018-10-02 17:17:09 -0600

    looping triangle

--- test/test.test.js
+++ test/test.test.js
@@ -3,9 +3,9 @@ import { stringSearch } from "../algorithms/search/stringSearch";
 describe("knuthMorrisPratt", () => {
   it("should find word position in given text", () => {
     expect(stringSearch("", "")).toBe(0);
+    expect(stringSearch("abcbcglx", "abca")).toBe(-1);
     expect(stringSearch("a", "")).toBe(0);
     expect(stringSearch("a", "a")).toBe(0);
-    expect(stringSearch("abcbcglx", "abca")).toBe(-1);
     expect(stringSearch("abcbcglx", "bcgl")).toBe(3);
     expect(stringSearch("abcxabcdabxabcdabcdabcy", "abcdabcy")).toBe(15);
     expect(stringSearch("abcxabcdabxabcdabcdabcy", "abcdabca")).toBe(-1);
@@ -17,3 +17,4 @@ describe("knuthMorrisPratt", () => {
     );
   });
 });
+SQ;
